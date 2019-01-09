7b09bab164b9f0a6e5a7bd2ed7e390929c392d63
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-09 20:49:39 +0000

    Redirects to an error route if user can't be authenticated

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -9,6 +9,33 @@ RSpec.feature 'User Authentication' do
       expect(page).to have_content 'Welcome, test@test.com'
     end
   end
+
+  context 'signup twice with the same email' do
+    scenario 'A user signs-up twice' do
+      visit '/'
+      click_on 'Sign up'
+      fill_in :email, with: 'test2@test.com'
+      fill_in :password, with: 'secret123'
+      click_on 'Sign up'
+      visit('/signup')
+      fill_in :email, with: 'test2@test.com'
+      fill_in :password, with: 'secret123'
+      click_button 'Sign up'
+      expect(page.current_path).to eq '/error'
+    end
+  end
+
+  
+  context 'signup with a password shorter then 6 characters' do
+    scenario 'A user try to signup with a short password' do
+      visit '/'
+      click_on 'Sign up'
+      fill_in :email, with: 'test2@test.com'
+      fill_in :password, with: 'secret'
+      click_on 'Sign up'
+      expect(page.current_path).to eq '/signup'
+    end
+  end
   
   context 'Sign in/out' do
   let!(:user) { User.create(email: 'test@test.com', password: 'secret123')}
@@ -34,18 +61,6 @@ RSpec.feature 'User Authentication' do
       expect(page).to have_content 'Sign in'
     end
   end
-  context 'signup twice with the same email' do
-    scenario 'A user signs-up twice' do
-      visit '/'
-      click_on 'Sign up'
-      fill_in :email, with: 'test2@test.com'
-      fill_in :password, with: 'secret123'
-      click_on 'Sign up'
-      visit('/signup')
-      fill_in :email, with: 'test2@test.com'
-      fill_in :password, with: 'secret123'
-      click_button 'Sign up'
-      expect(page.current_path).to eq '/error'
-    end
-  end
+  
+  
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -6,19 +6,19 @@ require './lib/user.rb'
     let!(:user) { User.create(email: "carol@gmail.com", password: "secret1234") }
     let(:user2) { User.create(email: "carol@gmail.com", password: "secret1234") }
     
-    describe '#authenticate' do
-      it 'user and password exist' do
-       expect(User.authenticate("carol@gmail.com", "secret1234")).to eq user
-      end
-    
-      it 'user doesnt exist' do
-       expect(User.authenticate("james@gmail.com", "1234")).to eq nil
-      end
-    
-      it 'user exists but password incorrect' do
-       expect(User.authenticate(user.email, "5678")).to eq nil
-      end
-    end
+   describe '#authenticate' do
+     it 'user and password exist' do
+      expect(User.authenticate("carol@gmail.com", "secret1234")).to eq user
+     end
+   
+     it 'user doesnt exist' do
+      expect(User.authenticate("james@gmail.com", "1234")).to eq nil
+     end
+   
+     it 'user exists but password incorrect' do
+      expect(User.authenticate(user.email, "5678")).to eq nil
+     end
+   end
     
   describe '#valid?' do
     it 'checks if the user email already exists' do
